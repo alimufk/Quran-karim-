46d31584b3564e787a14f7c0c052c8a039cadb61
ali <ahmadmufk445@gmail.com>
2026-07-16 13:06:08 +0300

    Refactor main.tsx to remove LanguageProvider
    Removed LanguageProvider from the rendering tree.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+Import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import { LanguageProvider } from './context/LanguageContext.tsx'; // استيراد نظام اللغة المطور
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
+    <App />
   </StrictMode>,
 );
 
