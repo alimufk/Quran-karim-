1ea8c616e301a007348a5e3e7162eeff86f48065
ali <ahmadmufk445@gmail.com>
2026-08-02 22:54:38 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/example/alquran/MainActivity.java
+++ android/app/src/main/java/com/example/alquran/MainActivity.java
@@ -1,5 +1,30 @@
-package com.example.alquran;
+package com.example.alquran
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Bundle
+import com.getcapacitor.BridgeActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.media.AudioAttributes
+import android.net.Uri
+import android.os.Build
 
-public class MainActivity extends BridgeActivity {}
+class MainActivity : BridgeActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Adhan Channel"
+            val descriptionText = "Channel for Adhan Notifications"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel("adhan_channel", name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
