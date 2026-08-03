e338769ee7780ed13ad15ffecbc725cc9a765d71
ali <ahmadmufk445@gmail.com>
2026-08-03 21:16:12 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/example/alquran/MainActivity.java
+++ android/app/src/main/java/com/example/alquran/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.media.AudioAttributes;
+import android.net.Uri;
 import android.os.Build;
 
 public class MainActivity extends BridgeActivity {
@@ -15,13 +17,22 @@ public class MainActivity extends BridgeActivity {
 
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Uri soundUri = Uri.parse("android.resource://" + getPackageName() + "/raw/adhan");
+            
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .build();
+
             String name = "Adhan Channel";
             String descriptionText = "Channel for Adhan Notifications";
             int importance = NotificationManager.IMPORTANCE_HIGH;
-            
+
             NotificationChannel channel = new NotificationChannel("adhan_channel", name, importance);
             channel.setDescription(descriptionText);
-            
+            channel.setSound(soundUri, audioAttributes);
+            channel.enableVibration(true);
+
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             if (notificationManager != null) {
                 notificationManager.createNotificationChannel(channel);
