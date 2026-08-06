7678d060cc1fcf6c7e2b183c1311d946fd74f745
ali <ahmadmufk445@gmail.com>
2026-08-06 23:27:27 +0300

    Update AdhanReceiver.java

--- android/app/src/main/java/com/example/alquran/AdhanReceiver.java
+++ android/app/src/main/java/com/example/alquran/AdhanReceiver.java
@@ -3,14 +3,16 @@ package com.example.alquran;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
+import androidx.core.content.ContextCompat;
 
 public class AdhanReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent serviceIntent = new Intent(context, AdhanService.class);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            context.startForegroundService(serviceIntent);
+        
+        // تشغيل السيرفس بأعلى أولوية في الخلفية
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            ContextCompat.startForegroundService(context, serviceIntent);
         } else {
             context.startService(serviceIntent);
         }
