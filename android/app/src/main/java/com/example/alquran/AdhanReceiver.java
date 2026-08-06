6f1c22cb7b6b56ae9a03a7182a3af4cce35c1328
ali <ahmadmufk445@gmail.com>
2026-08-07 00:04:59 +0300

    Remove comment on service priority in AdhanReceiver
    Removed comment about running the service with high priority in the background.

--- android/app/src/main/java/com/example/alquran/AdhanReceiver.java
+++ android/app/src/main/java/com/example/alquran/AdhanReceiver.java
@@ -10,7 +10,6 @@ public class AdhanReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Intent serviceIntent = new Intent(context, AdhanService.class);
         
-        // تشغيل السيرفس بأعلى أولوية في الخلفية
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             ContextCompat.startForegroundService(context, serviceIntent);
         } else {
